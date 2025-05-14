85f4ab4bc67d57066db49c39644e3137852d64cb
AppOutlet team <team.appoutlet@gmail.com>
2025-05-14 18:56:07 +0100

    setup icons

--- website/src/app/app.component.ts
+++ website/src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { Component } from "@angular/core";
+import { HeaderComponent } from "./header/header.component";
+import { MatIconModule, MatIconRegistry } from "@angular/material/icon";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, MatToolbarModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  selector: "app-root",
+  imports: [HeaderComponent, MatIconModule],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'website';
+  constructor(iconRegistry: MatIconRegistry) {
+    iconRegistry.registerFontClassAlias("ph", "ph")
+  }
 }
