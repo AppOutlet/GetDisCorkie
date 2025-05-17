978532baf602dbd0390ed6f14f7c3b2898975b97
AppOutlet team <team.appoutlet@gmail.com>
2025-05-17 21:08:11 +0100

    download button

--- website/src/app/hero/hero.component.ts
+++ website/src/app/hero/hero.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [MatIconModule, MatButtonModule],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
