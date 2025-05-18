69b82d4d72fec7e0d57073b5916032fae1124921
AppOutlet team <team.appoutlet@gmail.com>
2025-05-18 18:05:22 +0100

    social buttons

--- website/src/app/hero/hero.component.ts
+++ website/src/app/hero/hero.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { SocialButtonComponent } from '../social-button/social-button.component';
 
 @Component({
   selector: 'app-hero',
-  imports: [MatIconModule, MatButtonModule],
+  imports: [MatIconModule, MatButtonModule, SocialButtonComponent],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
