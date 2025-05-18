6d5d642354e64ad966c36be9a9a0e2483830b9e7
AppOutlet team <team.appoutlet@gmail.com>
2025-05-18 18:17:46 +0100

    slogan section

--- website/src/app/app.component.ts
+++ website/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from "@angular/core";
 import { HeaderComponent } from "./header/header.component";
 import { MatIconModule, MatIconRegistry } from "@angular/material/icon";
 import { HeroComponent } from "./hero/hero.component";
+import { SloganComponent } from "./slogan/slogan.component";
 
 @Component({
   selector: "app-root",
-  imports: [HeaderComponent, MatIconModule, HeroComponent],
+  imports: [HeaderComponent, MatIconModule, HeroComponent, SloganComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })
