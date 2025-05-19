e15e311a38bb216af78aba5a9cfca7d3d9638272
AppOutlet team <team.appoutlet@gmail.com>
2025-05-19 18:59:09 +0100

    fetch posts

--- website/src/app/app.component.ts
+++ website/src/app/app.component.ts
@@ -1,19 +1,28 @@
-import { Component } from "@angular/core";
-import { HeaderComponent } from "./header/header.component";
-import { MatIconModule, MatIconRegistry } from "@angular/material/icon";
-import { HeroComponent } from "./hero/hero.component";
-import { SloganComponent } from "./slogan/slogan.component";
-import { FeaturesComponent } from "./features/features.component";
-import { DownloadComponent } from "./download/download.component";
+import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { HeroComponent } from './hero/hero.component';
+import { SloganComponent } from './slogan/slogan.component';
+import { FeaturesComponent } from './features/features.component';
+import { DownloadComponent } from './download/download.component';
+import { PostsComponent } from './posts/posts.component';
 
 @Component({
-  selector: "app-root",
-  imports: [HeaderComponent, MatIconModule, HeroComponent, SloganComponent, FeaturesComponent, DownloadComponent],
-  templateUrl: "./app.component.html",
-  styleUrl: "./app.component.scss",
+  selector: 'app-root',
+  imports: [
+    HeaderComponent,
+    MatIconModule,
+    HeroComponent,
+    SloganComponent,
+    FeaturesComponent,
+    DownloadComponent,
+    PostsComponent,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   constructor(iconRegistry: MatIconRegistry) {
-    iconRegistry.registerFontClassAlias("ph", "ph")
+    iconRegistry.registerFontClassAlias('ph', 'ph');
   }
 }
