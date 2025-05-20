ae3170cf10831045a89d73f2e5cbdc8daccca1ba
AppOutlet team <team.appoutlet@gmail.com>
2025-05-20 17:07:57 +0100

    footer final form

---
+++ website/src/app/license-agreement/license-agreement.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-license-agreement',
+  imports: [],
+  templateUrl: './license-agreement.component.html',
+  styleUrl: './license-agreement.component.scss'
+})
+export class LicenseAgreementComponent {
+
+}
