b31d5a519781011085906e89f70339006b521e49
AppOutlet team <team.appoutlet@gmail.com>
2025-05-20 17:13:30 +0100

    update windows link on download component

--- website/src/app/download/download.component.ts
+++ website/src/app/download/download.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DownloadButtonComponent } from './download-button/download-button.component';
+import { DownloadService } from './download.service';
 
 @Component({
   selector: 'app-download',
   imports: [DownloadButtonComponent],
   templateUrl: './download.component.html',
-  styleUrl: './download.component.scss'
+  styleUrl: './download.component.scss',
 })
-export class DownloadComponent {
+export class DownloadComponent implements OnInit {
+  windowsDownloadLink: string = '';
 
+  constructor(private readonly downloadService: DownloadService) {}
+
+  ngOnInit(): void {
+    this.downloadService.getWindowsDownloadLink().subscribe((link) => {
+      this.windowsDownloadLink = link;
+    });
+  }
 }
