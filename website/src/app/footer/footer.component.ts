a28496aa3d8390cbf7808792739c5e84b19336ad
AppOutlet team <team.appoutlet@gmail.com>
2025-05-20 16:00:43 +0100

    get windows link

--- website/src/app/footer/footer.component.ts
+++ website/src/app/footer/footer.component.ts
@@ -1,13 +1,29 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
+import { DownloadService } from '../download/download.service';
 
 @Component({
   selector: 'app-footer',
   imports: [MatIconModule, MatButtonModule, CommonModule, RouterModule],
+  providers: [],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
-export class FooterComponent {}
+export class FooterComponent implements OnInit {
+  windowsDownloadLink: string = '';
+
+  constructor(private readonly dowloadService: DownloadService) { }
+
+  ngOnInit(): void {
+    this.getWindowsDownloadLink()
+  }
+
+  private getWindowsDownloadLink() {
+    return this.dowloadService.getWindowsDownloadLink().subscribe((link) => {
+      this.windowsDownloadLink = link;
+    });
+  }
+}
