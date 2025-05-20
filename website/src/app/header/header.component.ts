fac249018b7016fa4c6416731dc23b459d2aaae4
AppOutlet team <team.appoutlet@gmail.com>
2025-05-20 18:27:25 +0100

    header improvements

--- website/src/app/header/header.component.ts
+++ website/src/app/header/header.component.ts
@@ -2,6 +2,8 @@ import { Component } from "@angular/core";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatIconModule, MatIconRegistry } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
+import { MatMenuModule } from "@angular/material/menu";
+import { RouterModule } from "@angular/router";
 
 @Component({
   selector: "app-header",
@@ -9,6 +11,8 @@ import { MatButtonModule } from "@angular/material/button";
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
+    MatMenuModule,
+    RouterModule,
   ],
   templateUrl: "./header.component.html",
   styleUrl: "./header.component.scss",
