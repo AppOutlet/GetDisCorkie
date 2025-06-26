9943c6f588cd69668403503bc01f5552692699c1
AppOutlet team <team.appoutlet@gmail.com>
2025-06-26 16:16:40 +0100

    feat: add markdown rendering to privacy policy page
    - Added ngx-remark for rendering markdown content.
    - Updated privacy-policy.component.html to include <remark> component.
    - Modified privacy-policy.component.ts to import RemarkModule and define markdownContent.
    - Updated package.json to include dependencies for katex, ngx-remark, and remark.

--- website/src/app/privacy-policy/privacy-policy.component.ts
+++ website/src/app/privacy-policy/privacy-policy.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { RemarkModule } from 'ngx-remark';
 
 @Component({
   selector: 'app-privacy-policy',
   templateUrl: './privacy-policy.component.html',
-  styleUrl: './privacy-policy.component.scss'
+  styleUrl: './privacy-policy.component.scss',
+  imports: [RemarkModule],
 })
 export class PrivacyPolicyComponent {
   markdownContent = '# Hello world';
-
-
 }
